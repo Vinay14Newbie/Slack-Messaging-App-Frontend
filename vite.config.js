cbaa1fc63197e15469b7647d4e2824d9b5dcd833
Vinay14Newbie <wjust1984@gmail.com>
2024-12-19 14:52:22 +0530

    Added eslint setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), //we're giving absolute path here
-    },
-  },
+      '@': path.resolve(__dirname, './src') //we're giving absolute path here
+    }
+  }
 });
