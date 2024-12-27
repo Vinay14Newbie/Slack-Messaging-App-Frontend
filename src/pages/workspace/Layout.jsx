3c783ad1f071891a936203806167b7be773dd456
Vinay14Newbie <wjust1984@gmail.com>
2024-12-27 13:54:51 +0530

    Added resizable panel for building channel list

--- src/pages/workspace/Layout.jsx
+++ src/pages/workspace/Layout.jsx
@@ -1,5 +1,10 @@
 import { WorkspaceNavbar } from "@/components/organisms/workspace/WorkspaceNavbar";
 import { WorkspaceSidebar } from "@/components/organisms/workspace/WorkspaceSidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export const WorkspaceLayout = ({ children }) => {
   return (
@@ -7,7 +12,23 @@ export const WorkspaceLayout = ({ children }) => {
       <WorkspaceNavbar />
       <div className="flex h-[calc(100vh-40px)]">
         <WorkspaceSidebar />
-        {children}
+
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId={"workspace-resize"}
+        >
+          <ResizablePanel
+            defaultSize={50}
+            minSize={11}
+            className="bg-slack-medium"
+          >
+            <div className="flex h-[200px] items-center justify-center p-6">
+              <span className="font-semibold">One</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
