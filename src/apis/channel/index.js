6bca8c20b2bb9f0d13199800f302a5c3aefe238b
Vinay14Newbie <wjust1984@gmail.com>
2025-01-02 20:04:38 +0530

    Added get channel by id query api

---
+++ src/apis/channel/index.js
@@ -0,0 +1,14 @@
+import axios from "@/config/axiosConfig";
+
+export const getChannelByIdRequest = async ({ token, channelId }) => {
+  try {
+    const response = await axios.get(`/channels/${channelId}`, {
+      headers: { "x-access-token": token },
+    });
+    console.log("Response of get channel by id: ", response);
+    return response?.data?.data;
+  } catch (error) {
+    console.log("Error while fetching a channel by id: ", error);
+    throw error.response.data;
+  }
+};
---
+++ src/hooks/apis/channel/useGetChannelById.js
@@ -0,0 +1,22 @@
+import { getChannelByIdRequest } from "@/apis/channel";
+import { useAuth } from "@/hooks/context/useAuth";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetChannelById = (channelId) => {
+  const { auth } = useAuth();
+  const {
+    isFetching,
+    isSuccess,
+    error,
+    data: channel,
+  } = useQuery({
+    queryFn: () => getChannelByIdRequest({ channelId, token: auth?.token }),
+    queryKey: [`getChannel-${channelId}`],
+  });
+  return {
+    isFetching,
+    isSuccess,
+    error,
+    channel,
+  };
+};
