6449d741435424e45b790562e121225090c71dda
Vinay14Newbie <wjust1984@gmail.com>
2025-01-02 21:45:30 +0530

    Added basic quill setup

--- src/pages/workspace/channel/Channel.jsx
+++ src/pages/workspace/channel/Channel.jsx
@@ -1,6 +1,32 @@
+import { ChatInput } from "@/components/molecules/chatInput/ChatInput";
+import { useGetChannelById } from "@/hooks/apis/channel/useGetChannelById";
+import { Loader2Icon, TriangleAlertIcon } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 export const Channel = () => {
   const { channelId } = useParams();
-  return <div>Channel {channelId}</div>;
+  const { channelDetails, isFetching, error } = useGetChannelById(channelId);
+
+  if (isFetching) {
+    return (
+      <div className="h-full flex flex-1 items-center justify-center">
+        <Loader2Icon className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="h-full flex flex-1 flex-col gap-y-2 items-center justify-center">
+        <TriangleAlertIcon className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel Not Found</span>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-1" />
+      <ChatInput />
+    </div>
+  );
 };
